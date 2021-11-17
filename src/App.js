5415e9924a98fef5ce87b7cc3e83e3e1595dbf66
meghanadeep <Meghanadeep.Kumble@walmartlabs.com>
2021-11-17 17:57:25 +0530

    fresh branch to demo live

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import Home from './components/Home';
-import CarouselComp from './pages/carouselComp';
-import AccordianComp from './pages/accordianComp';
-import InfiniteScrollComp from './pages/infiniteScrollComp';
-import DocumentTreeComp from './pages/documentTreeComp';
-import ModalComp from './pages/ModalComp';
-
-
-
 import './App.css';
 
 function App() {
   return (
     <div className='App'>  
-     <ModalComp/>
+     <Home/>
     </div>
   );
 }
